4b2c7c3123eed639275d9ef43f3d01834562c70b
LinneaMichel <linneamichel@gmail.com>
2015-10-15 15:19:01 +0200

    allt möjligt

---
+++ jq.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+ $(function () {
+  $("#bokhel").hover(function () {
+    this.src="images/bok3.jpg";
+  }, function () {
+    this.src="images/bok4.jpg";
+  });});
+
+
+
+});
