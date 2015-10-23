872392169db5f263228cdf1b6f0fa28009d05ebe
LinneaMichel <linneamichel@gmail.com>
2015-10-23 16:21:31 +0200

    Jupp

---
+++ scripts/jqsub.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+     	$(function () {
+  $("#old1").hover(function () {
+    this.src="images/old/old3.jpg";
+  }, function () {
+    this.src="images/old/old1.jpg";
+  });});
+
+
+     	$(function () {
+  $("#old1deutsch").hover(function () {
+    this.src="../images/old/old3.jpg";
+  }, function () {
+    this.src="../images/old/old1.jpg";
+  });});
+
+
+
+
+
+
+
+});
